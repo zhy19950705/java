c86c7f6c2b4c0994537a2945a11389e244771625
zhy <Administrator@0QEYXNUZOV81QRB>
2017-07-26 16:08:42 +0800

    no message

---
+++ src/yxlm/Hero.java
@@ -0,0 +1,79 @@
+package yxlm;
+
+public class Hero {
+	private String name;
+	private float hp;
+	private float hujia;
+	private float mokang;
+	private float damage;
+	private String speakWhenKill;
+	private String speakWhenDead;
+
+	public void keng() {
+		System.out.println("坑队友！");
+	}
+
+	public void legenderay() {
+		System.out.println("玩家" + name + "超神了！");
+	}
+
+	public void recovery(float blood) {
+		hp += blood;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public float getHp() {
+		return hp;
+	}
+
+	public void setHp(float hp) {
+		this.hp = hp;
+	}
+
+	public float getHujia() {
+		return hujia;
+	}
+
+	public void setHujia(float hujia) {
+		this.hujia = hujia;
+	}
+
+	public float getMokang() {
+		return mokang;
+	}
+
+	public void setMokang(float mokang) {
+		this.mokang = mokang;
+	}
+
+	public float getDamage() {
+		return damage;
+	}
+
+	public void setDamage(float damage) {
+		this.damage = damage;
+	}
+
+	public String getSpeakWhenKill() {
+		return speakWhenKill;
+	}
+
+	public void setSpeakWhenKill(String speakWhenKill) {
+		this.speakWhenKill = speakWhenKill;
+	}
+
+	public String getSpeakWhenDead() {
+		return speakWhenDead;
+	}
+
+	public void setSpeakWhenDead(String speakWhenDead) {
+		this.speakWhenDead = speakWhenDead;
+	}
+}
---
+++ src/yxlm/Mangzai.java
@@ -0,0 +1,14 @@
+package yxlm;
+
+public class Mangzai {
+	public static void main(String[] args) {
+		Hero Mangzai = new Hero();
+		Mangzai.setName("李青");
+		Mangzai.setHp(570);
+        Mangzai.setDamage(10);
+        Mangzai.setSpeakWhenDead("小锋锋的抠脚瞎");
+        Mangzai.setSpeakWhenKill("小晗晗真厉害");
+        System.out.println(Mangzai.getName()+"\n"+Mangzai.getHp()+"\n"+Mangzai.getDamage()+"\n"+Mangzai.getSpeakWhenDead()+"\n"+Mangzai.getSpeakWhenKill());
+	}
+
+}
